07721aef5dea79b72df8b65f54e80313d6f25911
Tim Schuster <mail@timschuster.info>
2016-08-22 21:15:20 +0200

    Added Examples

---
+++ examples/client/hello_client.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"git.eyecikjou567.de/go/lsep"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		client()
+	}
+}
+
+func client() {
+
+	fmt.Printf("Dialing localhost...\n")
+
+	socket, err := lsep.NewTCPConnection("127.0.0.1:13338")
+
+	if err != nil {
+		fmt.Printf("Error while dialing: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Reading from Socket\n")
+
+	hello, err := socket.Read()
+
+	if err != nil {
+		fmt.Printf("Error while reading: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Received: %s\n", hello)
+
+	fmt.Printf("Reading from Socket\n")
+
+	t1 := time.Now()
+
+	test, err := socket.Read()
+
+	t2 := time.Now().Sub(t1)
+
+	if err != nil {
+		fmt.Printf("Error while reading: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Received: %d bytes\n", len(test))
+	fmt.Printf("Took    : %s\n", t2)
+	fmt.Printf("Speed   : %f GiBps\n", float64(len(test))/(t2.Seconds()*1024*1024*1024))
+
+	socket.Close()
+}
---
+++ examples/server/hello_server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"git.eyecikjou567.de/go/lsep"
+)
+
+func main() {
+
+	var toSend = ""
+	var toSendBuffer bytes.Buffer
+	fmt.Printf("Generating...")
+	for i := 0; i < 500*1024*1024; i++ {
+		if i%(200*1024*1024) == 0 {
+			fmt.Printf("%d kb...", i/1024)
+		}
+		toSendBuffer.WriteByte(0xFF)
+	}
+
+	toSend = string(toSendBuffer.Bytes())
+
+	fmt.Println("Done!")
+
+	listener, err := lsep.NewTCPListener("127.0.0.1:13338")
+
+	if err != nil {
+		fmt.Printf("Error while listening: %s", err)
+		return
+	}
+
+	for {
+		fmt.Printf("Waiting for listeners...\n")
+		socket, err := listener.Accept()
+
+		fmt.Printf("New listener!\n")
+
+		if err != nil {
+			fmt.Printf("Error while establishing connection: %s", err)
+			return
+		}
+
+		fmt.Printf("Beginning Data Session...\n")
+
+		hello := []byte("Hello World")
+
+		fmt.Printf("Sending %d bytes\n", len(hello))
+		err = socket.Write(hello)
+
+		if err != nil {
+			fmt.Printf("Error while sending: %s", err)
+			return
+		}
+
+		fmt.Printf("Sending Second Part...\n")
+
+		err = socket.Write([]byte(toSend))
+
+		if err != nil {
+			fmt.Printf("Error while sending: %s\n", err)
+		}
+
+		fmt.Printf("Finished Session, closing...\n")
+
+		err = socket.Close()
+
+		if err != nil {
+			fmt.Printf("Error while closing: %s\n", err)
+		}
+	}
+}
